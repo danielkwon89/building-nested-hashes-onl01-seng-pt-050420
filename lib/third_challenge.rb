a8da7b73042b3284889570708a52909063be59f1
Daniel Kwon <danielkwon89@gmail.com>
2020-05-12 20:09:19 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,7 +18,8 @@ def third_challenge
      },
      capulet: {
        patriarch: {
-         
+         name: "Lord Capulet",
+         age: "50"
        },
        matriarch: {},
        heroine: {},
